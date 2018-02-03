ea910850585dc206ce55f6fb0c65cb2e233a0f5f
0l3shka <q0l3shka@gmail.com>
2018-02-03 11:29:12 +0700

    +mnain

--- ObjectOverrides/Program.cs
+++ ObjectOverrides/Program.cs
@@ -6,10 +6,27 @@ using System.Threading.Tasks;
 
 namespace ObjectOverrides
 {
+    class Person { }
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("****** Fun with System.Object *****\n");
+            Person p1 = new Person();
+
+            Console.WriteLine("ToString: {0}", p1.ToString());
+            Console.WriteLine("Hash code: {0}", p1.GetHashCode());
+            Console.WriteLine("Type: {0}", p1.GetType());
+
+            Person p2 = p1;
+            object o = p2;
+
+            if (o.Equals(p1) && p2.Equals(o))
+            {
+                Console.WriteLine("Same instance!");
+            }
+
+            Console.ReadLine();
         }
     }
 }
