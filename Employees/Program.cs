546089ad5b7fbaee6a79bd8ab2b6a24d1981c6f6
0l3shka <q0l3shka@gmail.com>
2018-02-03 11:20:01 +0700

    GivePromotion test, as

--- Employees/Program.cs
+++ Employees/Program.cs
@@ -8,6 +8,27 @@ namespace Employees
 {
     class Program
     {
+        static void CastingExamples()
+        {
+            object frank = new Manager("Frank Zappa", 9, 3000, 40000, "111-11-1111", 5);
+            Employee moonUnit = new Manager("MoonUnit Zappa", 2, 3001, 20000, "101-11-1321", 1);
+            GivePromotion(moonUnit);
+            Salesperson jill = new PTSalesPerson("Jill", 834, 3002, 100000, "111-12-1119", 90,4);
+            PTSalesPerson TestPTSP = new PTSalesPerson("TestPTSP", 22, 99, 20000, "13337", 20, 4);
+
+            GivePromotion((Manager)frank);
+            GivePromotion((Salesperson)TestPTSP);
+
+            Salesperson sp = frank as Salesperson;
+            if (sp == null)
+                Console.WriteLine("Sorry, frank is not a salesperson...");
+        }
+
+        static void GivePromotion(Employee emp)
+        {
+            Console.WriteLine("{0} was promoted!", emp.Name);
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("***** The Employee Class Hierarchy ******\n");
@@ -18,6 +39,9 @@ namespace Employees
             Salesperson fran = new Salesperson("Fran", 43, 93, 3000, "932-32-3232", 31);
             fran.GiveBonus(200);
             fran.DisplayStats();
+
+            CastingExamples();
+
             Console.ReadLine();
            
         }
--- Shapes/Program.cs
+++ Shapes/Program.cs
@@ -11,11 +11,14 @@ namespace Shapes
         static void Main(string[] args)
         {
             Console.WriteLine("***** Fun with Polymorphism ******\n");
-            Hexagon hex = new Hexagon("Beth");
-            hex.Draw();
-            Circle cir = new Circle("Cindy");
-            cir.Draw();
+            Shape[] myShapes = { new Hexagon(), new Circle(), new Hexagon("Mick"), new Circle("Beth"), new Hexagon("Linda") };
+
+            foreach (Shape s in myShapes)
+            {
+                s.Draw();
+            }
             Console.ReadLine();
         }
+       
     }
 }
