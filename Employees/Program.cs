b7b135c75742cc696dce12923ac78662247b9a43
0l3shka <q0l3shka@gmail.com>
2018-02-03 10:41:08 +0700

    +Main

--- Employees/Program.cs
+++ Employees/Program.cs
@@ -10,7 +10,15 @@ namespace Employees
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("***** The Employee Class Hierarchy ******\n");
+            Manager chucky = new Manager("Chucky", 50, 92, 10000, "333-23-23222", 9000);
+            chucky.GiveBonus(300);
+            chucky.DisplayStats();
+            Console.WriteLine();
+            Salesperson fran = new Salesperson("Fran", 43, 93, 3000, "932-32-3232", 31);
+            fran.GiveBonus(200);
+            fran.DisplayStats();
+            Console.ReadLine();
         }
     }
 }
